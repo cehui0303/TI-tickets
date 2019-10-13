47ae7cec1124f49dd3cf9546571ab4fd07ed4445
cehui0303 <cehui0303@163.com>
2019-10-13 14:56:45 +0800

    修复chrome中获取异步加载的dom元素

--- script.js
+++ script.js
@@ -1,20 +1,125 @@
+
+//定时器
+var timer = null;
+
+var _isSetSku = false;
+
+//选中档次
+function chooseSku(){
+	var sku = document.getElementsByClassName('sku_item')[2];
+	
+	if(sku){
+		sku.click();
+		
+		if(timer){
+			clearTimeout(timer);
+		}
+		
+		var buybtn = document.getElementsByClassName('buybtn')[0];
+	
+		if( buybtn.className.indexOf("disabled")> -1 ){
+			//表示还没到抢票时
+			console.log('刷票中...');
+			setTimeout(function() {
+					window.location.reload();
+				}, 500);
+		}else{
+			
+			//票的数量  如果还不可以购买，这个地方获取会失败 
+			var amount = document.getElementsByClassName('cafe-c-input-number-handler-up')[0];
+			amount && amount.click();  //+1
+		}
+
+		//提交购买
+		buybtn.click();
+		
+	}else{
+		console.log('等待异步数据加载中....');
+		//自我调用
+		timer = setTimeout(chooseSku,500);
+		
+	}
+}
+
+//选择观影人
+function chooseUser(){
+	var user_info = document.querySelectorAll('.ticket-buyer-title span')[0];
+		
+	if(user_info.className.indexOf('buyer-enough')>-1){
+		
+		if(timer){
+			clearTimeout(timer);
+		}
+		
+		var btn_submit = document.querySelectorAll('.submit-wrapper button')[0];
+
+		//提交
+		//btn_submit && btn_submit.click();
+		//debugger;
+		if(btn_submit){
+			//alert('提交订单成功！');
+			console.log('提交订单成功！');
+		}
+	}else{
+		
+		console.log('观影人:等待异步数据加载中....');
+		
+		//自我调用
+		timer = setTimeout(chooseUser,500);
+	}
+}
+
+//提交订单
+function submitOrder(){
+	//选择人员
+	var user2 = document.querySelectorAll('.ticket-buyer-select input')[1];
+	user2.click(); chooseUser();return;
+	if(user2){
+		user2.click();
+		
+		
+		
+		if(timer){
+			clearTimeout(timer);
+		}
+		
+		chooseUser();
+		
+		
+	}else{
+		
+		console.log('等待异步数据加载中....');
+		
+		//自我调用
+		timer = setTimeout(submitOrder,500);
+	}
+	
+	
+	
+	
+	
+}
+
 var grab = function() {
     
+	var date = new Date();
+	
+	//12:16开始刷
+	//if(date.getHours() < 12 ) return;
+	//if(date.getMinutes() < 16 ) return;
 	
 	var href = window.location.href;
 	
 	//如果是提交订单页面
 	if(href.indexOf('buy.damai.cn/orderConfirm') > -1 ){
 		
-		//选择人员
-		var user2 = document.querySelectorAll('.ticket-buyer-select input')[1];
-		user2.click();
+		console.log('提交订单');
 		
-		var btn_submit = document.querySelectorAll('.submit-wrapper button')[0];
-
-		btn_submit.click();
+		submitOrder();
 		
-		console.log('提交成功！');
+		
+		
+		//console.log('提交成功！');
 		
 		return;
 	}
@@ -26,19 +131,8 @@ var grab = function() {
 	//$('.sku_item:nth-child(4)').click();
 	
 	//选中档次
-	var sku = document.getElementsByClassName('sku_item')[3];
-	sku.click();
-	
-	//票的数量 
-	var amount = document.getElementsByClassName('cafe-c-input-number-handler-up')[0];
-	amount.click();
-	
-	//提交购买
-	var buybtn = document.getElementsByClassName('buybtn')[0];
-	buybtn.click();
-	
-	
-	
+	chooseSku();
+
 	console.log('OK!');
 	
 	return;
@@ -71,5 +165,8 @@ var grab = function() {
         setTimeout('grab()', 1000);
     }
 	*/
+	
 }
+
+
 grab();
